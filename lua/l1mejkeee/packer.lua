a025bad966aaeb213c6727f0b26b2c7a3616b437
Nikita <avstriyskiy.ni@ed-it.ru>
2023-11-03 21:18:41 +0300

    added comment

--- lua/l1mejkeee/packer.lua
+++ lua/l1mejkeee/packer.lua
@@ -23,21 +23,19 @@ return require('packer').startup(function(use)
         'OscarCreator/rsync.nvim',
         run = 'make',
         requires = {'nvim-lua/plenary.nvim'},
-        config = function()
-            require("rsync").setup()
-        end
     }
+    use "terrortylor/nvim-comment"
     use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	}
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 end)
