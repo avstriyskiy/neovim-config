44883b7df85b66c8f626c7eb96a65df721a37437
avstriyskiy <avstriyskiy_ni@ed_it.ru>
2023-11-03 17:51:02 +0300

    added rsync.neovim and deleted autoclose

--- lua/l1mejkeee/packer.lua
+++ lua/l1mejkeee/packer.lua
@@ -1,27 +1,33 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.3',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-	use (
-	'nvim-treesitter/nvim-treesitter',
-	{run = ':TSUpdate'}
-	)
+    use (
+    'nvim-treesitter/nvim-treesitter',
+    {run = ':TSUpdate'}
+    )
 
-	use ('theprimeagen/harpoon')
-	use ('mbbill/undotree')
-	use ('tpope/vim-fugitive')
-
-	use 'm4xshen/autoclose.nvim'
-	use {
+    use ('theprimeagen/harpoon')
+    use ('mbbill/undotree')
+    use ('tpope/vim-fugitive')
+    use {
+        'OscarCreator/rsync.nvim',
+        run = 'make',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = function()
+            require("rsync").setup()
+        end
+    }
+    use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
 		requires = {
