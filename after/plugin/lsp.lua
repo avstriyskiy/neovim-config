d6d193958221f5cd4584e17aae588c77e853e7f3
Nikita Avstriyskiy <n.avstriyskiy@yandex.ru>
2023-12-20 18:18:46 +0300

    deleted unneccessary files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,8 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	'rust_analyzer',
-	'phpactor',
 	'eslint',
 	'tsserver',
-	'pyright',
 })
 
 local cmp = require('cmp')
--- after/plugin/phpactor.lua
+++
@@ -1,7 +0,0 @@
-require'lspconfig'.phpactor.setup{
-    init_options = {
-        ["language_server_phpstan.enabled"] = false,
-        ["language_server_psalm.enabled"] = false,
-    }
-}
-
