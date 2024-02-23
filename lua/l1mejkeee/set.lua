db7ef208176d4f9ff0f1099bd4d5a23c72bb12d4
Nikita Avstriyskiy <smileneverdie@gmail.com>
2024-02-23 17:35:03 +0300

    changed harpoon key and added remembering pos

--- lua/l1mejkeee/set.lua
+++ lua/l1mejkeee/set.lua
@@ -36,3 +36,12 @@ vim.opt.updatetime = 50
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("WQ", "wq", {})
 vim.api.nvim_create_user_command("Q", "q", {})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = {"*"},
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.api.nvim_exec("normal! g'\"",false)
+        end
+    end
+})
