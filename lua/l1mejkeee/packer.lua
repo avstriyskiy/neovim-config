6acdae254cdff92cbea7501ec2e9b6dc9e559cbb
Nikita Avstriyskiy <smileneverdie@gmail.com>
2024-03-16 18:44:16 +0300

    added lualine and sonokai theme

--- lua/l1mejkeee/packer.lua
+++ lua/l1mejkeee/packer.lua
@@ -1,37 +1,43 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+    use "wbthomason/packer.nvim"
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        "nvim-telescope/telescope.nvim", tag = "0.1.3",
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { {"nvim-lua/plenary.nvim"} }
     }
 
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-    -- use 'AlexvZyl/nordic.nvim'
-
-    use (
-    'nvim-treesitter/nvim-treesitter',
-    {run = ':TSUpdate'}
-    )
-
-    use ('theprimeagen/harpoon')
-    use ('mbbill/undotree')
-    use ('tpope/vim-fugitive')
-    use "terrortylor/nvim-comment"
+    -- Neovim beautify
+    use({ "rose-pine/neovim", as = "rose-pine" }) 
+    use "sainnhe/sonokai"
     use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true }
+    }
+    
+    -- Extras
+    use "theprimeagen/harpoon"
+    use "mbbill/undotree"
+    use "terrortylor/nvim-comment"
+    
+    -- Git integration
+    use ("tpope/vim-fugitive")
+    use "lewis6991/gitsigns.nvim"
+    
+    -- LSP and Treesitter
+    use ("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
+    use {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         requires = {
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            {"neovim/nvim-lspconfig"},             -- Required
+            {"williamboman/mason.nvim"},           -- Optional
+            {"williamboman/mason-lspconfig.nvim"}, -- Optional
+            {"hrsh7th/nvim-cmp"},     -- Required
+            {"hrsh7th/cmp-nvim-lsp"}, -- Required
+            {"L3MON4D3/LuaSnip"},     -- Required
         }
     }
 end)
